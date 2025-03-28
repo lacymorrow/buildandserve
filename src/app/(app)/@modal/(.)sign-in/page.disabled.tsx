9e23d0683561b427dea138aa858bb07271c3aeaa
Lacy Morrow <me@lacymorrow.com>
2025-03-27 22:43:46 -0400

    auto commit

--- src/app/(app)/@modal/(.)sign-in/page.disabled.tsx
+++ src/app/(app)/@modal/(.)sign-in/page.disabled.tsx
@@ -2,11 +2,15 @@
 
 import { SignIn } from "@/app/(app)/(authentication)/sign-in/_components/sign-in";
 import { Modal } from "@/components/primitives/modal";
+import { SuspenseFallback } from "@/components/primitives/suspense-fallback";
+import { Suspense } from "react";
 
 export default async function Page() {
 	return (
 		<Modal dialogTitle="Sign in">
-			<SignIn />
+			<Suspense fallback={<SuspenseFallback />}>
+				<SignIn />
+			</Suspense>
 		</Modal>
 	);
 }
