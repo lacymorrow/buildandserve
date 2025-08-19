ee1fcaf859ae8e330748cfb069b53378d8afc1c2
Lacy Morrow <me@lacymorrow.com>
2025-08-19 17:56:16 -0400

    auto commit (#84)

--- src/components/ui/theme.tsx
+++ src/components/ui/theme.tsx
@@ -31,9 +31,10 @@
 
 "use client";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { ThemeProvider, useTheme } from "next-themes";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 import React from "react";
 import { Button, type ButtonProps } from "@/components/ui/button";
+import { env } from "@/env";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -53,18 +54,25 @@ ThemeButton.displayName = "ThemeButton";
 const ThemeToggle = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
 	const { theme, setTheme } = useTheme();
 
-	return (
-		<ThemeButton
-			onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-			{...props}
-			ref={ref}
-		/>
-	);
+	const lightEnabled = !!env.NEXT_PUBLIC_FEATURE_LIGHT_MODE_ENABLED;
+	const darkEnabled = !!env.NEXT_PUBLIC_FEATURE_DARK_MODE_ENABLED;
+	const canToggle = lightEnabled && darkEnabled;
+
+	function handleClick() {
+		if (!canToggle) return;
+		setTheme(theme === "light" ? "dark" : "light");
+	}
+
+	return <ThemeButton onClick={handleClick} disabled={!canToggle} {...props} ref={ref} />;
 });
 ThemeToggle.displayName = "ThemeToggle";
 
 const ThemeChooser = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
 	const { setTheme } = useTheme();
+
+	const lightEnabled = !!env.NEXT_PUBLIC_FEATURE_LIGHT_MODE_ENABLED;
+	const darkEnabled = !!env.NEXT_PUBLIC_FEATURE_DARK_MODE_ENABLED;
+	const bothEnabled = lightEnabled && darkEnabled;
 
 	return (
 		<DropdownMenu>
@@ -72,13 +80,45 @@ const ThemeChooser = React.forwardRef<HTMLButtonElement, ButtonProps>((props, re
 				<ThemeButton {...props} ref={ref} />
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+				{lightEnabled && (
+					<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+				)}
+				{darkEnabled && (
+					<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+				)}
+				{bothEnabled && (
+					<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+				)}
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
 });
 ThemeChooser.displayName = "ThemeChooser";
+
+// Wrapper ThemeProvider that enforces allowed themes based on build-time flags
+// while preserving a compatible interface with next-themes' ThemeProvider
+const ThemeProvider = ({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) => {
+	const lightEnabled = !!env.NEXT_PUBLIC_FEATURE_LIGHT_MODE_ENABLED;
+	const darkEnabled = !!env.NEXT_PUBLIC_FEATURE_DARK_MODE_ENABLED;
+	const bothEnabled = lightEnabled && darkEnabled;
+	const computedThemes = [lightEnabled && "light", darkEnabled && "dark"].filter(Boolean) as string[];
+	const themes = computedThemes.length ? computedThemes : ["light"];
+	const defaultTheme = bothEnabled ? "system" : lightEnabled ? "light" : "dark";
+	const forcedTheme = bothEnabled ? undefined : (lightEnabled ? "light" : "dark");
+
+	return (
+		<NextThemesProvider
+			attribute="class"
+			enableSystem={bothEnabled}
+			forcedTheme={forcedTheme}
+			defaultTheme={defaultTheme}
+			themes={themes}
+			disableTransitionOnChange
+			{...props}
+		>
+			{children}
+		</NextThemesProvider>
+	);
+};
 
 export { ThemeChooser, ThemeProvider, ThemeToggle };
